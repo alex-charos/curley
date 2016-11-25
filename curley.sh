cebe430ed6e2a65afff9e4159cf21b85c0efc3c6
Alex <realtowz@yahoo.com>
2016-11-25 14:01:59 +0200

    comments added

--- curley.sh
+++ curley.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# Simple script that calls a list of urls and outputs the result
+# urls.txt required with one url per line
 for i in $(cat urls.txt); do
 	content="$(curl -s "$i")"
 	echo "$content" >> results.txt
