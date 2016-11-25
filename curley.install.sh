f669e4c7d69a721659b362bf041397c38507ff9c
Alex <realtowz@yahoo.com>
2016-11-25 15:51:54 +0200

    directory creation, copy of shell script, fixed crontab entry

--- curley.install.sh
+++ curley.install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+DIRECTORY=~/.curley/
 crontab -l > mycron
 gre=$(grep "curley.sh" "mycron")
 if [ ${#gre} -eq  0 ]
 then
+	echo $DIRECTORY
+	if [ -d $DIRECTORY ]
+	then
+		#mdkir $DIRECTORY
+		echo $DIRECTORY exists
+	else
+		mkdir -p "$DIRECTORY"
+ 		echo $DIRECTORY created		
+	fi
+	cp curley.sh $DIRECTORY
+	echo curley.sh copied
 	echo "will add cronjob"
-	echo "0,30 * * * * curley.sh" >> mycron
+	echo "0,30 * * * * "$DIRECTORY"curley.sh" >> mycron
 	crontab mycron
 fi
 rm mycron
