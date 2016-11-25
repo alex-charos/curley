27ddc3a00d44c075719f65f16b56a25b2eeba79b
Alex <realtowz@yahoo.com>
2016-11-25 17:55:27 +0200

    proper date echo

--- curley.sh
+++ curley.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Simple script that calls a list of urls and outputs the result
 # urls.txt required with one url per line
-echo date "Starting..."
+echo $(date) "Starting..."
 
 for i in $(cat $1); do
 	content="$(curl -I "$i")"
 	echo $i "Called!"
 done
-echo date "Finished..."
+echo $(date) "Finished..."
